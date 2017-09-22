9d51d69c07f6bcdfbde91a888cf35806d1d2d0d2
marieantoinetteontopoftheworld <jack.heseltine@gmail.com>
2017-09-22 19:15:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,19 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase();
 }
+
+function logShout(string) {
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
+
+function sayHiToGrandma(string) {
+  if (string.lowercase) {
+    return "I can't hear you!"
+  } else {
+    return "YES INDEED!"
+  }
+}
